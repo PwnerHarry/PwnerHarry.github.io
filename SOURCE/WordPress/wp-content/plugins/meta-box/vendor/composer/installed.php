956ca9a0748df4c8e98df597c88726871e14ca5c
Harry Zhao <mingdeharry@outlook.com>
2024-01-16 17:27:21 -0500

    20240116 17h27

--- SOURCE/WordPress/wp-content/plugins/meta-box/vendor/composer/installed.php
+++ SOURCE/WordPress/wp-content/plugins/meta-box/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'wpmetabox/meta-box',
-        'pretty_version' => '5.8.2',
-        'version' => '5.8.2.0',
-        'reference' => 'de583a74171bdee1b5213232a5bf50f82c6fee14',
+        'pretty_version' => '5.9.1',
+        'version' => '5.9.1.0',
+        'reference' => '9c32b609e4eaee3c08fb9905b53bb437f2a84e19',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'wpmetabox/meta-box' => array(
-            'pretty_version' => '5.8.2',
-            'version' => '5.8.2.0',
-            'reference' => 'de583a74171bdee1b5213232a5bf50f82c6fee14',
+            'pretty_version' => '5.9.1',
+            'version' => '5.9.1.0',
+            'reference' => '9c32b609e4eaee3c08fb9905b53bb437f2a84e19',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

9999c4e3c4e2648e5209181e4779e7b2e1ec219a
Harry Zhao <mingdeharry@outlook.com>
2024-03-25 16:14:21 -0400

    fix upload

--- SOURCE/homepage.sql
+++ SOURCE/homepage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 25, 2024 at 08:08 PM
+-- Generation Time: Mar 25, 2024 at 08:13 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -469,7 +469,7 @@ INSERT INTO `wp_postmeta` (`meta_id`, `post_id`, `meta_key`, `meta_value`) VALUE
 (196, 36, '_edit_lock', '1711397261:1'),
 (197, 62, '_wp_attached_file', '2020/06/no_revision.jpg'),
 (198, 62, '_wp_attachment_metadata', 'a:5:{s:5:\"width\";i:720;s:6:\"height\";i:372;s:4:\"file\";s:23:\"2020/06/no_revision.jpg\";s:5:\"sizes\";a:2:{s:6:\"medium\";a:4:{s:4:\"file\";s:23:\"no_revision-300x155.jpg\";s:5:\"width\";i:300;s:6:\"height\";i:155;s:9:\"mime-type\";s:10:\"image/jpeg\";}s:9:\"thumbnail\";a:4:{s:4:\"file\";s:23:\"no_revision-150x150.jpg\";s:5:\"width\";i:150;s:6:\"height\";i:150;s:9:\"mime-type\";s:10:\"image/jpeg\";}}s:10:\"image_meta\";a:12:{s:8:\"aperture\";s:1:\"0\";s:6:\"credit\";s:0:\"\";s:6:\"camera\";s:0:\"\";s:7:\"caption\";s:0:\"\";s:17:\"created_timestamp\";s:1:\"0\";s:9:\"copyright\";s:0:\"\";s:12:\"focal_length\";s:1:\"0\";s:3:\"iso\";s:1:\"0\";s:13:\"shutter_speed\";s:1:\"0\";s:5:\"title\";s:0:\"\";s:11:\"orientation\";s:1:\"0\";s:8:\"keywords\";a:0:{}}}'),
-(199, 65, '_edit_lock', '1711397038:1'),
+(199, 65, '_edit_lock', '1711397431:1'),
 (544, 566, '_wp_attached_file', '2024/03/CV.pdf'),
 (432, 358, '_wp_attached_file', '2022/02/Haiper.png'),
 (431, 84, '_wp_old_date', '2020-06-15'),
